6003d93d1d1909fd03116d812e94a1043f9b0b68
jaderson2103 <tavaresaffonso@hotmail.com>
2016-05-30 14:14:39 -0300

    Update ImpostoTest.java

--- test/jenkins/ImpostoTest.java
+++ test/jenkins/ImpostoTest.java
@@ -3,6 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+ 
+ /**
+ *
+ * @author trollador
+ */
+ 
 package jenkins;
 
 import org.junit.After;
@@ -12,57 +18,9 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author trollador
- */
 
-/**
-public class ImpostoTest {
-    
-    public ImpostoTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
-    /**
-     * Test of calcula method, of class Imposto.
-     */
-  
-/**
-@Test
-    public void testCalcula() {
-        System.out.println("calcula");
-        Orcamento orcamento = null;
-        Imposto instance = new Imposto();
-        double expResult = 0.0;
-        double result = instance.calcula(orcamento);
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
-}
-*/
 
-/**
- *
- * @author trollador
- */
 import jenkins.Imposto;
 import jenkins.Orcamento;
 import org.junit.Assert;
